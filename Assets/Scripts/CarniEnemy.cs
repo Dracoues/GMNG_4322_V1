94b2c69ceb2ef1791b1b12b3d5c3225c13fe1728
Elijah Ray <eray502@outlook.com>
2025-12-15 02:29:10 -0600

    Create CarniEnemy.cs

---
+++ Assets/Scripts/CarniEnemy.cs
@@ -0,0 +1,279 @@
+
+using System.Collections;
+
+using UnityEditor.Rendering;
+
+using UnityEngine;
+
+
+
+public class CarniEnemy : MonoBehaviour, IDamageable
+
+{
+
+
+
+    #region Variables
+
+    public CarniBaseState currentState;
+
+
+
+    public PatrolState patrolState;
+
+    public PlayerDetectedState playerDetectedState;
+
+    public CarniChargeState carniChargeState;
+
+    public CarniAttackState carniAttackState;
+
+    public DamagedState damagedState;
+
+
+
+    public Animator anim;
+
+    public Rigidbody2D rb;
+
+    public Transform ledgeDetector;
+
+    public Transform stegoDetector;
+
+    public LayerMask groundLayer, playerLayer, damageableLayer, enemyLayer, carniLayer;
+
+    public TagHandle carniTag;
+
+
+
+    public int facingDirection = 1;
+
+    public float stateTime;         //keep track of the time when we enter a new state
+
+
+
+    public EnemyStats stats;
+
+    public float currentHealth;
+
+
+
+    public float stegoCounter;
+
+    
+
+
+
+    #endregion
+
+
+
+    #region Unity Callbacks
+
+
+
+    private void Awake()
+
+    {
+
+        patrolState = new PatrolState(this, "patrol");
+
+        playerDetectedState = new PlayerDetectedState(this, "playerDetected");
+
+        carniChargeState = new CarniChargeState(this, "charge");
+
+        carniAttackState = new CarniAttackState(this, "attack");
+
+        damagedState = new DamagedState(this, "damaged");
+
+
+
+        currentState = patrolState; 
+
+        currentState.Enter();
+
+    }
+
+
+
+    void Start()
+
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+        currentHealth = stats.maxHealth;
+
+    }
+
+
+
+    private void Update()
+
+    {
+
+        currentState.LogicUpdate();
+
+    }
+
+
+
+    void FixedUpdate()
+
+    {
+
+        currentState.PhysicsUpdate();
+
+        
+
+    }
+
+    #endregion
+
+
+
+    #region Checks
+
+    public bool CheckLedgesAndWallsAndCarnis()
+
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(ledgeDetector.position, Vector2.down, stats.cliffCheckDistance, groundLayer);
+
+        RaycastHit2D wallhit = Physics2D.Raycast(ledgeDetector.position, Vector2.right, stats.wallDistance, groundLayer);
+
+
+
+        if (hit.collider == null || wallhit.collider == true)
+
+        {
+
+            return true;
+
+        }
+
+        else
+
+            return false;
+
+
+
+    }
+
+
+
+    public bool CheckForPlayer()
+
+    {
+
+        RaycastHit2D hitPlayer = Physics2D.Raycast(ledgeDetector.position, facingDirection == 1 ? Vector2.right : Vector2.left, stats.playerDetectDistance, playerLayer);
+
+
+
+        if (hitPlayer.collider == true)
+
+        {
+
+            return true;
+
+        }
+
+        else
+
+            return false;
+
+    }
+
+
+
+    public bool CheckForMeleeTarget()
+
+    {
+
+        RaycastHit2D hitMeleeTarget = Physics2D.Raycast(ledgeDetector.position, facingDirection == 1 ? Vector2.right : Vector2.left, stats.meleeDetectDistance, playerLayer);
+
+
+
+        if (hitMeleeTarget.collider == true)
+
+        {
+
+            return true;
+
+        }
+
+        else
+
+            return false;
+
+    }
+
+
+
+    #endregion
+
+
+
+    #region Other Functions
+
+
+
+    public void SwitchState(CarniBaseState newState)
+
+    {
+
+        currentState.Exit();
+
+        currentState = newState;
+
+        currentState.Enter();
+
+        stateTime = Time.time;
+
+    }
+
+
+
+    public void AnimationFinsihedTrigger()
+
+    {
+
+        currentState.AnimationFinishedTigger();
+
+    }
+
+    public void AnimationAttackTrigger()
+
+    {
+
+        currentState.AnimationAttackTrigger();
+
+
+
+    }
+
+
+
+    public void Damage(float damageAmount){ }
+
+
+
+    public void Damage(float damageAmount, float KBForce, Vector2 KBAngle)
+
+    {
+
+        damagedState.KBForce = KBForce;
+
+        damagedState.KBAngle = KBAngle;
+
+        SwitchState(damagedState);
+
+        currentHealth -= damageAmount;
+
+    }
+
+
+
+    #endregion
+
+}
+
