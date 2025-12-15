eddb277ceeb5c9e3ccbb4dbd5d9303e16c41272a
Elijah Ray <eray502@outlook.com>
2025-12-15 03:00:10 -0600

    Create CarniDamagedState.cs

---
+++ Assets/Scripts/CarniDamagedState.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+
+
+public class DamagedState : CarniBaseState
+
+{
+
+    public float KBForce;
+
+    public Vector2 KBAngle;
+
+
+
+    public DamagedState(StegoEnemy stego, string animationName) : base(stego, animationName)
+
+    {
+
+
+
+    }
+
+
+
+    public override void Enter()
+
+    {
+
+        base.Enter();
+
+        ApplyKnockback();
+
+
+
+    }
+
+
+
+    public override void Exit()
+
+    {
+
+        base.Exit();
+
+    }
+
+
+
+    public override void LogicUpdate()
+
+    {
+
+        base.LogicUpdate();
+
+    }
+
+
+
+    public override void PhysicsUpdate()
+
+    {
+
+        base.PhysicsUpdate();
+
+    }
+
+
+
+    public override void AnimationAttackTrigger()
+
+    {
+
+        base.AnimationAttackTrigger();
+
+    }
+
+
+
+    public override void AnimationFinishedTigger()
+
+    {
+
+        base.AnimationFinishedTigger();
+
+    }
+
+
+
+    private void ApplyKnockback()
+
+    {
+
+        stego.rb.linearVelocity = KBAngle * KBForce;
+
+    }
+
+
+
+}
+
