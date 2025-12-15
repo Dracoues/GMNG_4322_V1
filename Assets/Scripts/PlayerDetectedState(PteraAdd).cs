4d1aff42c33584ba04fafba271cf096d7cfcb1b0
Elijah Ray <eray502@outlook.com>
2025-12-15 02:10:02 -0600

    Update PlayerDetectedState(Ptera&CarniAdd).cs

--- Assets/Scripts/PlayerDetectedState(PteraAdd).cs
+++ Assets/Scripts/PlayerDetectedState(PteraAdd).cs
@@ -72,7 +72,75 @@ public class PlayerDetectedState : StegoBaseState
 
 }
 
+public class PlayerDetectedState : CarniBaseState
 
+{
+
+    public PlayerDetectedState(CarniEnemy carni, string animationName) : base(carni, animationName)
+
+    {    }
+
+
+
+    public override void Enter()
+
+    {
+
+        base.Enter();
+
+        Debug.Log("Entered PlayerDetected");
+
+    }
+
+
+
+    public override void Exit()
+
+    {
+
+        base.Exit();
+
+        Debug.Log("Exited PlayerDetected");
+
+        // carni.rb.linearVelocity = Vector2.zero;
+
+    }
+
+    public override void LogicUpdate()
+
+    {
+
+        base.LogicUpdate();
+
+
+
+        if (!carni.CheckForPlayer())
+
+            carni.SwitchState(carni.patrolState);
+
+        else
+
+        {
+
+            if (Time.time >= carni.stateTime + carni.stats.playerDetectedWaitTime)
+
+                carni.SwitchState(carni.carniChargeState);
+
+        }
+
+    }
+
+
+
+    public override void PhysicsUpdate()
+
+    {
+
+        base.PhysicsUpdate();
+
+    }
+
+}
 
 public class PlayerDetectedState : PteraBaseState
 
