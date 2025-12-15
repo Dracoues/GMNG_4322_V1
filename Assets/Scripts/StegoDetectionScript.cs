dfbf01a42a0b566d2af113279ed3fce58cedc323
Elijah Ray <eray502@outlook.com>
2025-12-15 14:27:21 -0600

    Update StegoDetectionScript.cs

--- Assets/Scripts/StegoDetectionScript.cs
+++ Assets/Scripts/StegoDetectionScript.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
 public class StegoDetectionScript : MonoBehaviour
@@ -5,9 +7,22 @@ public class StegoDetectionScript : MonoBehaviour
 
     public float stegoCount;
 
-    void Update()
-    {
-        
 
+    public TransformBlock detectionPoint;
+    public float detectionRadius = 5f;
+    public LayerMask stegoLayer;
+
+    public int stegoIndex()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(detectionPoint.position, detectionRadius, stegoLayer);
+
+        return hitColliders.Length;
+    }
+
+    public bool CarniAggro(int stegoCount <= 2)
+    {
+        int count = stegoIndex();
+        Debug.Log(count);
+        return count > stegoCount;
     }
 }
