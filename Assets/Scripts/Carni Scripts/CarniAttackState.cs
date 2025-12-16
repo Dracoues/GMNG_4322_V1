7ef6b5b6b7ebcbb486745835f1a707c86877658e
Elijah Ray <eray502@outlook.com>
2025-12-15 20:23:25 -0600

    Update CarniAttackState.cs

--- Assets/Scripts/Carni Scripts/CarniAttackState.cs
+++ Assets/Scripts/Carni Scripts/CarniAttackState.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
 
-
-
 public class CarniAttackState : CarniBaseState
 
 {
-
     public CarniAttackState(CarniEnemy carni, string animationName) : base(carni, animationName)
 
-    {
-
-    }
-
-
+    {  }
 
     public override void Enter()
 
     {
-
         base.Enter();
+        //start cooldown
+        carni.lastAttackTime = Time.time;
+        //stop movement
+        carni.rb.linearVelocity = Vector2.zero;
+        //trigger animation
+        carni.anim.SetTrigger("Attack");
+        
         Debug.Log("Entered Attack");
     }
 
@@ -27,56 +26,43 @@ public class CarniAttackState : CarniBaseState
     public override void Exit()
 
     {
-
         base.Exit();
         Debug.Log("Exited Attack");
-
     }
-
-
 
     public override void LogicUpdate()
 
-    {
-        base.LogicUpdate();
-    }
-
-
+    { base.LogicUpdate(); }
 
     public override void PhysicsUpdate()
 
     {
         base.PhysicsUpdate();
+
+        if (carni.CheckForMeleeTarget() && carni.stegoDetector.CarniAggro())
+        {
+        carni.SwitchState(carni.carniAttackState);
+        AnimationAttackTrigger();
+        }
     }
-
-
 
     public override void AnimationAttackTrigger()
 
     {
-
         base.AnimationAttackTrigger();
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(carni.ledgeDetector.position, carni.stats.meleeDetectDistance, carni.damageableLayer);
 
-
-
         foreach (Collider2D hitCollider in hitColliders)
 
-        {
-
-            IDamageable damageable = hitCollider.GetComponent<IDamageable>();
-
-
+        {IDamageable damageable = hitCollider.GetComponent<IDamageable>();
 
             if ((damageable != null))
 
-            {
+            { hitCollider.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(carni.stats.knockbackAngle.x * carni.facingDirection,
 
-                hitCollider.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(carni.stats.knockbackAngle.x * carni.facingDirection,
+              carni.stats.knockbackAngle.y) * carni.stats.knockbackForce;
 
-                    carni.stats.knockbackAngle.y) * carni.stats.knockbackForce;
-
-                damageable.Damage(carni.stats.damageAmount);
+              damageable.Damage(carni.stats.damageAmount);
 
             }
 
@@ -84,19 +70,12 @@ public class CarniAttackState : CarniBaseState
 
     }
 
-
-
     public override void AnimationFinishedTigger()
 
-    {
-
-        base.AnimationFinishedTigger();
-
+    {   base.AnimationFinishedTigger();
         carni.SwitchState(carni.patrolState);
 
     }
-
-
 
 }
 
