aeeb9e13ac09f344e732a2e729d5f4505a45bad0
Elijah Ray <eray502@outlook.com>
2025-12-15 20:40:47 -0600

    Update CarniPlayerDetectedState.cs

--- Assets/Scripts/Carni Scripts/CarniPlayerDetectedState.cs
+++ Assets/Scripts/Carni Scripts/CarniPlayerDetectedState.cs
@@ -5,19 +5,13 @@ public class CarniPlayerDetectedState : CarniBaseState
 {
 
     public CarniPlayerDetectedState(CarniEnemy carni, string animationName) : base(carni, animationName)
-
     {    }
-
-
 
     public override void Enter()
 
     {
-
         base.Enter();
-
         UnityEngine.Debug.Log("Entered PlayerDetected");
-
     }
 
 
@@ -25,48 +19,29 @@ public class CarniPlayerDetectedState : CarniBaseState
     public override void Exit()
 
     {
-
         base.Exit();
-
         UnityEngine.Debug.Log("Exited PlayerDetected");
-
         // carni.rb.linearVelocity = Vector2.zero;
-
     }
 
     public override void LogicUpdate()
 
     {
-
         base.LogicUpdate();
 
-
-
         if (!carni.CheckForPlayer())
-
             carni.SwitchState(carni.patrolState);
-
         else
-
         {
-
             if (UnityEngine.Time.time >= carni.stateTime + carni.stats.playerDetectedWaitTime)
-
                 carni.SwitchState(carni.carniChargeState);
-
         }
 
     }
 
-
-
     public override void PhysicsUpdate()
 
-    {
-
-        base.PhysicsUpdate();
-
-    }
+    { base.PhysicsUpdate(); }
 
 }
 
